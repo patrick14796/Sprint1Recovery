b02510be8b912b2f7125c02590bb8a707e232864
patrick14796 <57112628+patrick14796@users.noreply.github.com>
2021-03-30 22:31:09 +0300

    bugs fix

--- Src/index.js
+++ Src/index.js
@@ -7,48 +7,47 @@ const bodyParser= require("body-parser")
 const MongoClient = require("mongodb").MongoClient
 
 MongoClient.connect("mongodb+srv://ivan:!Joni1852!@cluster0.vb8as.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{useUnifiedTopology:true}).then(client => {
-console.log("Connected to Database")
-const db = client.db("login-auth")
-const loginData=db.collection("loginData")
-app.use(bodyParser.urlencoded({ extended: true }))
-app.post("/auth",(req,res) => {
+    console.log("Connected to Database")
+	const db = client.db("login-auth")
+    const loginData=db.collection("loginData")
+	app.use(bodyParser.urlencoded({ extended: true }))
+	app.post("/auth",(req,res) => {
 	
-var user_name = req.body.user_name;
-var password = req.body.password;
+		var user_name = req.body.user_name;
+        var password = req.body.password;
 
-var data={
-"user":user_name,
-"password":password
-}
-db.collection("loginData").insertOne(data,function(err, collection){ 
-if (err) {
-throw err
-}; 
-console.log("Record inserted Successfully");       
-}); 
+        var data={
+            "user":user_name,
+            "password":password
+        }
+        db.collection("loginData").insertOne(data,function(err, collection){ 
+            if (err) {
+				throw err
+			}; 
+            console.log("Record inserted Successfully");       
+        }); 
 
-return res.sendFile(__dirname + "/loggedIn.html");
-})
+		return res.sendFile(__dirname + "/loggedIn.html");
+    })
 
-app.get("/", (req, res)=>{
-res.render("Homepage")
-res.status(200)
-})
+	app.get("/", (req, res)=>{
+		res.render("Homepage")
+		res.status(200)
+	})
 	
-app.get("/Login", (req, res)=>{
-res.render("Login")
-})
+	app.get("/Login", (req, res)=>{
+		res.render("Login")
+	})
 	
-app.get("/Register", (req, res)=>{
-res.render("Register")
-})
+	app.get("/Register", (req, res)=>{
+		res.render("Register")
+	})
 	
-app.listen(port, ()=>{
-console.log("Listening to port 3000!!!")
-})
+	app.listen(port, ()=>{
+		console.log("Listening to port 3000!!!")
+	})
 	
 
 }).catch(console.error)
-
 
 
