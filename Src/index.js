c7cce53332f55505491909b90ce97dff7ad93d13
patrick14796 <57112628+patrick14796@users.noreply.github.com>
2021-03-30 22:38:02 +0300

    tab prob

--- Src/index.js
+++ Src/index.js
@@ -3,50 +3,50 @@ const port = process.env.PORT || 3000
 const app = express()
 app.set("view engine", "ejs")
 app.use(express.static("public"))
-const bodyParser= require("body-parser")
+const bodyParser = require("body-parser")
 const MongoClient = require("mongodb").MongoClient
 
-MongoClient.connect("mongodb+srv://ivan:!Joni1852!@cluster0.vb8as.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{useUnifiedTopology:true}).then(client => {
-    console.log("Connected to Database")
+MongoClient.connect("mongodb+srv://ivan:!Joni1852!@cluster0.vb8as.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useUnifiedTopology: true }).then(client => {
+	console.log("Connected to Database")
 	const db = client.db("login-auth")
-    const loginData=db.collection("loginData")
+	const loginData = db.collection("loginData")
 	app.use(bodyParser.urlencoded({ extended: true }))
-	app.post("/auth",(req,res) => {
-	
-		var user_name = req.body.user_name;
-        var password = req.body.password;
+	app.post("/auth", (req, res) => {
 
-        var data={
-            "user":user_name,
-            "password":password
-        }
-        db.collection("loginData").insertOne(data,function(err, collection){ 
-            if (err) {
+		var user_name = req.body.user_name;
+		var password = req.body.password;
+
+		var data = {
+			"user": user_name,
+			"password": password
+		}
+		db.collection("loginData").insertOne(data, function (err, collection) {
+			if (err) {
 				throw err
-			}; 
-            console.log("Record inserted Successfully");       
-        }); 
+			};
+			console.log("Record inserted Successfully");
+		});
 
 		return res.sendFile(__dirname + "/loggedIn.html");
-    })
+	})
 
-	app.get("/", (req, res)=>{
+	app.get("/", (req, res) => {
 		res.render("Homepage")
 		res.status(200)
 	})
-	
-	app.get("/Login", (req, res)=>{
+
+	app.get("/Login", (req, res) => {
 		res.render("Login")
 	})
-	
-	app.get("/Register", (req, res)=>{
+
+	app.get("/Register", (req, res) => {
 		res.render("Register")
 	})
-	
-	app.listen(port, ()=>{
+
+	app.listen(port, () => {
 		console.log("Listening to port 3000!!")
 	})
-	
+
 
 }).catch(console.error)
 
