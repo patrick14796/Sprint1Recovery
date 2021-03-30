a6ae41667c2fbafd4d20af138abd7b15dfb8efd3
patrick14796 <57112628+patrick14796@users.noreply.github.com>
2021-03-30 22:27:25 +0300

    try Fix

--- Src/index.js
+++ Src/index.js
@@ -3,15 +3,15 @@ const port = process.env.PORT || 3000
 const app = express()
 app.set("view engine", "ejs")
 app.use(express.static("public"))
-const bodyParser= require('body-parser')
-const MongoClient = require('mongodb').MongoClient
+const bodyParser= require("body-parser")
+const MongoClient = require("mongodb").MongoClient
 
-MongoClient.connect('mongodb+srv://ivan:!Joni1852!@cluster0.vb8as.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{useUnifiedTopology:true}).then(client => {
+MongoClient.connect("mongodb+srv://ivan:!Joni1852!@cluster0.vb8as.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{useUnifiedTopology:true}).then(client => {
 console.log("Connected to Database")
 const db = client.db("login-auth")
-const loginData=db.collection('loginData')
+const loginData=db.collection("loginData")
 app.use(bodyParser.urlencoded({ extended: true }))
-app.post('/auth',(req,res) => {
+app.post("/auth",(req,res) => {
 	
 var user_name = req.body.user_name;
 var password = req.body.password;
@@ -20,14 +20,14 @@ var data={
 "user":user_name,
 "password":password
 }
-db.collection('loginData').insertOne(data,function(err, collection){ 
+db.collection("loginData").insertOne(data,function(err, collection){ 
 if (err) {
 throw err
 }; 
 console.log("Record inserted Successfully");       
 }); 
 
-return res.sendFile(__dirname + '/loggedIn.html');
+return res.sendFile(__dirname + "/loggedIn.html");
 })
 
 app.get("/", (req, res)=>{
