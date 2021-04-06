58115e958cb983f3966cb5214c7036c43606db3f
colvan <yoniborisenco@gmail.com>
2021-04-06 17:32:22 +0300

    added examples for validation
    validation examples on server side
    and client side

--- public/public_add_new_contractor_worker/js/global.js
+++ public/public_add_new_contractor_worker/js/global.js
@@ -12,3 +12,30 @@ function validate() {
 	}
 	return( true );
 }
+
+(function ($) {
+    "use strict";
+
+
+    /*==================================================================
+    [ Focus input ]*/
+    $('[name="full_name"').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+				alert("this is an alert")
+                $(this).removeClass('has-val');
+            }
+        })    
+    })
+  
+  
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    validate(input)
+
+})(jQuery);
