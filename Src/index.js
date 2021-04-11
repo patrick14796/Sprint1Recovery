c045ead5d647c4400770643619fbd687fec09d84
RonaNazarov <Ronanazarov@gmail.com>
2021-04-11 14:04:58 +0300

    contractor worker homepage

--- Src/index.js
+++ Src/index.js
@@ -3,70 +3,11 @@ const port = process.env.PORT || 3000
 const app = express()
 app.set("view engine", "ejs")
 app.use(express.static("public"))
-const bodyParser = require("body-parser")
-const MongoClient = require("mongodb").MongoClient
-
-MongoClient.connect("mongodb+srv://ivan:!Joni1852!@cluster0.vb8as.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useUnifiedTopology: true }).then(client => {
-	console.log("Connected to Database")
-	const db = client.db("login-auth")
-	const loginData = db.collection("loginData")
-	app.use(bodyParser.urlencoded({ extended: true }))
-	app.post("/auth", (req, res) => {
-
-		var user_name = req.body.user_name
-		var password = req.body.password
-
-		var data = {
-			"user": user_name,
-			"password": password
-		}
-		loginData.insertOne(data, function (err, collection) {
-			if (err) {
-				throw err
-			}
-			console.log("Record inserted Successfully" + collection.insertedCount)
-		})
-
-		return res.sendFile(__dirname + "/loggedIn.html")
-	})
-
-	app.get("/", (req, res) => {
-		res.render("Homepage")
-		res.status(200)
-	})
-
-	app.get("/Login", (req, res) => {
-		res.render("Login")
-	})
-
-	app.get("/Register", (req, res) => {
-		res.render("Register")
-	})
-
-	app.get("/contractor_worker_home_page", (req, res) => {
-		res.render("contractor_worker_home_page")
-	})
-
-	app.get("/contractor_worker_my_profile", (req, res) => {
-		res.render("contractor_worker_my_profile")
-	})
-
-	app.get("/contractor_worker_work_information", (req, res) => {
-		res.render("contractor_worker_work_information")
-	})
-
-	app.get("/contractor_worker_work_history", (req, res) => {
-		res.render("contractor_worker_work_history")
-	})
-
-	app.get("/contractor_worker_pay_rates", (req, res) => {
-		res.render("contractor_worker_pay_rates")
-	})
-
-	app.listen(port, () => {
-		console.log("Listening to port 3000!!!")
-	})
-
-}).catch(console.error)
 
 
+require("../Src/GET")(app)
+require("../Src/POST")(app)
+
+app.listen(port, () => {
+	console.log("Listening to port 3000!!!")
+})
--- public/public_contractor/my_profile.js
+++
@@ -1,3 +0,0 @@
-$(".card-header a").click(function(){
-    $(this).find('.icon').toggleClass('fa-plus fa-minus');
-});
