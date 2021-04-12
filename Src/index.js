5f2e719039b64b538ce111f26c866565ea83c90e
colvan <yoniborisenco@gmail.com>
2021-04-12 13:43:20 +0300

    updated careers page
    added careers page
    for now no careers at all

--- Src/index.js
+++ Src/index.js
@@ -68,6 +68,9 @@ MongoClient.connect("mongodb+srv://ivan:!Joni1852!@cluster0.vb8as.mongodb.net/my
 	app.get("/why_us_page", (req, res) => {
 		res.render("why_us_page")
 	})
+	app.get("/careers", (req, res) => {
+		res.render("careers_page")
+	})
 
 	app.get("/Register", (req, res) => {
 		res.render("Register")
