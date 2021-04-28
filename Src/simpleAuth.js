d786c4a7d68dd508bc9315db91d6e935cab259f3
AmitOvadia <amitovadia1210@gmail.com>
2021-04-28 20:32:51 +0300

    edit my profile contractor

--- Src/simpleAuth.js
+++ Src/simpleAuth.js
@@ -9,14 +9,10 @@ function authUser(req, res, next){
 
 function authRole(role){
 	return (req, res, next) => {
-		console.log(req.session.user.type)
-		console.log(role)
 		if(req.session.user.type !== role){
-			console.log("here")
 			res.status(401)
 			return res.send("Not allowed")
 		}
-		console.log("!!")
 		next()
 	}
 }
