74e5b28a72baaeba902c5f40f7a1540b8f91b760
patrick14796 <57112628+patrick14796@users.noreply.github.com>
2021-05-13 17:30:42 +0300

    After restore circleCi

--- public/public_contractor/contractor_calendar.js
+++ public/public_contractor/contractor_calendar.js
@@ -208,6 +208,18 @@ function updateLocalStorage() {
 
 
 
+	d=data[0].hiring
+    for(var i=0; i<d.length; ++i){
+      staticNotes.push({
+        id: d[i][0],
+
+            title: "Work Day",
+            desc: d[i][1]+ '\n' +d[i][2],
+            date: d[i][0]
+      });
+    }
+
+
     localStorage.setItem("notes", JSON.stringify(staticNotes));
     localStorage.setItem("notes", JSON.stringify(staticNotes));
 
