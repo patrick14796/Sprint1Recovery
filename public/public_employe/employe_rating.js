d0a5907d544fa0c9b38334b944eb21bd0435e4df
colvan <yoniborisenco@gmail.com>
2021-05-18 12:29:36 +0300

    update

--- public/public_employe/employe_rating.js
+++ public/public_employe/employe_rating.js
@@ -1,9 +1,3 @@
 $(document).ready(function() {
-var logID = 'log',
-  log = $('<div id="'+logID+'"></div>');
-$('body').append(log);
-  $('[type*="radio"]').change(function () {
-    var me = $(this);
-    log.html(me.attr('value'));
-  });
+
 });
